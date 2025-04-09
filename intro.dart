c4267bb2e665530d2f446f67df5618f89f81187b
Tokoyoda-hnut <tokoyoda.hnut@gmail.com>
2025-04-09 18:52:45 +0900

    ADD: function編まで

---
+++ intro.dart
@@ -0,0 +1,27 @@
+void main()
+{
+  Text('moji');
+  print(0);
+  MaterialApp false
+  List<String> nameList =  ['tanaka', 'okuno'];
+  print(nameList);
+
+  Map<String, Strin> nameMap = {
+    '1':'tanaka',
+    '2':'okuno'
+  };
+  print(nameMap);
+  print(nameMap[0]);
+
+  String name = 'okuno';
+  var num 2 = 0;
+  var nameList = [];
+
+  // function
+  void printNumber(int x) {
+  int number = 0;
+  print(number + x);
+  }
+  printNumber(5);
+
+}
